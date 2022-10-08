bf882d17607f465699f6eeb3e43abca44c664c86
coolara <772114880@qq.com>
2022-10-08 15:24:11 +0800

    'setting'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import PrismaModule from './prisma/prisma.module';
 import { SeedController } from './article/seed.controller';
+import { ArticleService } from './article/article.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [SeedController],
+  providers: [ArticleService],
 })
 export class AppModule {}
