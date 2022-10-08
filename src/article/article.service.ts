24173d49b477b2b7ad387a0bbff743d8ff324966
coolara <772114880@qq.com>
2022-10-08 14:52:45 +0800

    init commit

---
+++ src/article/article.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Article, ArticleCreateInput, ArticleUpdateInput } from '../types';
+
+@Injectable()
+export class ArticleService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createInput: ArticleCreateInput): Promise<Article> {
+    const res = await this.prisma.article.create({
+      data: createInput,
+    });
+
+    return res;
+  }
+
+  async update(updateInput: ArticleUpdateInput): Promise<Article> {
+    const res = await this.prisma.article.update({
+      data: updateInput,
+      where: {
+        id: updateInput.id,
+      },
+    });
+
+    return res;
+  }
+}
